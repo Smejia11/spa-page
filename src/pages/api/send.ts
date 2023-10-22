8bea3629c315bf5dfc6050957406825bf46541af
smejia-1 <santiago.mejia.oquendo1@gmail.com>
2023-10-22 01:07:39 -0500

    added instanceof

--- src/pages/api/send.ts
+++ src/pages/api/send.ts
@@ -30,6 +30,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
         return res.status(200).json(data);
     } catch (error) {
-        return res.status(500).json({ message: 'Se produjo un error inesperado al enviar el correo.', error: error.message });
+        if (error instanceof Error)
+            return res.status(500).json({ message: 'Se produjo un error inesperado al enviar el correo.', error: error.message });
     }
 }
