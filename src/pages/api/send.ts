4ed59930f22957c2494cf3d6e18013f78846facb
smejia-1 <santiago.mejia.oquendo1@gmail.com>
2023-10-22 01:04:12 -0500

    verificate enpoind

--- src/pages/api/send.ts
+++ src/pages/api/send.ts
@@ -7,9 +7,19 @@ const fromEmail: string | undefined = process.env.FROM_EMAIL;
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     try {
-        if (fromEmail === undefined)
-            return res.status(500).json({ message: 'Sufrimos un error inesperado vuelva a intentar mas tarde.' })
+        if (fromEmail === undefined) {
+            return res.status(500).json({ message: 'Sufrimos un error inesperado: el correo del remitente no está configurado.' });
+        }
+
+        if (req.method !== 'POST') {
+            return res.status(405).json({ message: 'Método no permitido. Utilice POST para enviar correos electrónicos.' });
+        }
+
         const { email, description, name } = req.body;
+
+        if (!email || !name || !description) {
+            return res.status(400).json({ message: 'Faltan campos obligatorios en la solicitud.' });
+        }
         // @ts-ignore
         const data = await resend.emails.send({
             from: fromEmail,
@@ -20,6 +30,6 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
         return res.status(200).json(data);
     } catch (error) {
-        return res.status(400).json(error);
+        return res.status(500).json({ message: 'Se produjo un error inesperado al enviar el correo.', error: error.message });
     }
 }
